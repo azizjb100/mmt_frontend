968e3bac2ddceaba8f9c72214bf444cd6c721cfb
Yahyakhaliman <yahyaindrayana35@gmail.com>
2026-02-20 09:45:47 +0700

    masterbahan & supplier

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { useAuthStore } from '@/stores/authStore';
 
 const api = axios.create({
-  baseURL: 'http://103.94.238.252:8003/api',
-  //baseURL: 'http://localhost:8003/api',
+  // baseURL: 'http://103.94.238.252:8003/api',
+  baseURL: 'http://localhost:8004/api',
   headers: {
     'Content-Type': 'application/json',
   },
