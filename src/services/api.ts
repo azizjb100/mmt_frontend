65f18d549b21f77e3db2729895f4cd99aab491ce
azizjb100 <azizjb100@gmail.com>
2026-02-21 09:17:45 +0700

    ok

---
+++ src/services/api.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { useAuthStore } from '@/stores/authStore';
+
+const api = axios.create({
+  baseURL: 'http://103.94.238.252:8003/api',
+  //baseURL: 'http://localhost:8003/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const authStore = useAuthStore();
+  if (authStore.token) {
+    config.headers.Authorization = `Bearer ${authStore.token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response?.status === 401) {
+      const authStore = useAuthStore();
+      authStore.logout();
+    }
+    return Promise.reject(err);
+  }
+);
+
+export default api;
